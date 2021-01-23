2111b8982445fd3306bb0d1b4ee572e11d1f116b
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-23 20:03:45 +0100

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <React.Fragment>
+      <section className="container">Hola mundo</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
